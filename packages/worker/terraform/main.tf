b3bbed679cad6d6b71d2b7e3c2452deca5a7a63e
Rahul Varghese <rahul.varghese@sourceplane.ai>
2026-05-23 08:50:50 +0530

    Revert "feat(packages/worker): Initial worker binding setup structure (#37)"
    This reverts commit 026c1e0abc0df11a035c9c4720641701c4d8e08e.

--- packages/worker/terraform/main.tf
+++
@@ -1,8 +0,0 @@
-# Worker infrastructure bindings
-# This component will provision necessary resources for worker applications
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
-
-# Worker secrets and bindings will be defined here
